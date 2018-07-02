3b0521a6e2f62651eb594a433d0615bd89262d60
Luis Castillo <luiscastillo@iwebsapp.com>
2018-07-02 12:55:31 -0500

    start proyects and cocoa pods

---
+++ EventsIOS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  EventsIOS
+//
+//  Created by Luis Castillo on 02/07/18.
+//  Copyright © 2018 Luis Castillo. All rights reserved.
+//
+
+import UIKit
+import Crashlytics
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func crasher(_ sender: UIButton) {
+        Crashlytics.sharedInstance().crash()
+    }
+    
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '10.0'
+
+target 'EventsIOS' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Envents
+  pod 'lottie-ios'
+  pod 'SwiftyJSON'
+  pod 'Alamofire'
+  pod 'SDWebImage'
+  pod 'RevealingSplashView'
+  pod 'Fabric'
+  pod 'Crashlytics'
+
+end
