474867bac365a9d0aaf2593630fd550a9a699618
Luis Castillo <luiscastillo@iwebsapp.com>
2018-07-24 23:52:09 -0500

    buttons map

--- EventsIOS/User/LayoutPlaces/Controller/LayoutPlacesController.swift
+++ EventsIOS/User/LayoutPlaces/Controller/LayoutPlacesController.swift
@@ -13,19 +13,20 @@ class LayoutPlacesController: UIViewController {
     @IBOutlet weak var mapView: UIView!
     @IBOutlet weak var newView: UIView!
     @IBOutlet weak var listView: UIView!
-    @IBOutlet weak var listenerNew: UIButton!
-    @IBOutlet weak var listenerList: UIButton!
-    private var contNew: Int = 0
-    private var contList: Int = 0
+    @IBOutlet weak var listenerMapButton: UIButton!
+    @IBOutlet weak var listenerListButton: UIButton!
+    @IBOutlet weak var listenerNewButton: UIButton!
+    private var conta: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        listenerNew.setImage(UIImage(named: "icon-add"), for: .normal)
-        listenerList.setImage(UIImage(named: "icon-list"), for: .normal)
+        //listenerNew.setImage(UIImage(named: "icon-add"), for: .normal)
+        //listenerList.setImage(UIImage(named: "icon-list"), for: .normal)
         mapView.alpha = 1
         newView.alpha = 0
         listView.alpha = 0
+        listenerMapButton.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,40 +34,40 @@ class LayoutPlacesController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func newEvent(_ sender: UIButton) {
-        if contNew == 0 {
-            listenerNew.setImage(UIImage(named: "menu-map-white"), for: .normal)
-            listenerList.isHidden = true
+    @IBAction func MapButton(_ sender: UIButton) {
+        listenerMapButton.isHidden = true
+        listenerListButton.isHidden = false
+        listenerNewButton.isEnabled = true
+        mapView.alpha = 1
+        newView.alpha = 0
+        listView.alpha = 0
+    }
+    
+    @IBAction func newButton(_ sender: UIButton) {
+        if conta == 0 {
+            listenerNewButton.setImage(UIImage(named: "menu-map-white"), for: .normal)
+            listenerListButton.isEnabled = false
             mapView.alpha = 0
             newView.alpha = 1
             listView.alpha = 0
-            contNew = 1
+            conta = 1
         } else {
-            listenerNew.setImage(UIImage(named: "icon-add"), for: .normal)
-            listenerList.isHidden = false
+            listenerNewButton.setImage(UIImage(named: "icon-add"), for: .normal)
+            listenerListButton.isEnabled = true
             mapView.alpha = 1
             newView.alpha = 0
             listView.alpha = 0
-            contNew = 0
+            conta = 0
         }
     }
     
-    @IBAction func listEvent(_ sender: UIButton) {
-        if contList == 0 {
-            listenerList.setImage(UIImage(named: "menu-map-white"), for: .normal)
-            listenerNew.isHidden = true
-            mapView.alpha = 0
-            newView.alpha = 0
-            listView.alpha = 1
-            contList = 1
-        } else {
-            listenerList.setImage(UIImage(named: "icon-list"), for: .normal)
-            listenerNew.isHidden = false
-            mapView.alpha = 1
-            newView.alpha = 0
-            listView.alpha = 0
-            contList = 0
-        }
+    @IBAction func listButton(_ sender: UIButton) {
+        listenerListButton.isHidden = true
+        listenerMapButton.isHidden = false
+        listenerNewButton.isEnabled = false
+        mapView.alpha = 0
+        newView.alpha = 0
+        listView.alpha = 1
     }
     
 }
