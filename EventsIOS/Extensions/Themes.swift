f6082dc1faeb915800302dbc301b58d9b005d8b9
Luis Castillo <luiscastillo@iwebsapp.com>
2018-07-26 17:36:50 -0500

    preparete theme

---
+++ EventsIOS/Extensions/Themes.swift
@@ -0,0 +1,34 @@
+//
+//  Themes.swift
+//  EventsIOS
+//
+//  Created by Luis Castillo on 26/07/18.
+//  Copyright © 2018 Luis Castillo. All rights reserved.
+//
+
+import UIKit
+
+class Themes {
+    
+    //themes day
+    static let headerGobalDay = UIImage(named: "header-2")
+    static let headerEventDay = UIImage(named: "headers")
+    static let backgroundLoginDay = UIImage(named: "wallpeper")
+    static let backgroundDay = UIColor(named: "backgroundDay")
+    static let tintColorDay = UIColor(named: "tintColorDay")
+    static let topBackgroundDay = UIColor(named: "topBackgroundDay")
+    static let bottomBackgroundDay = UIColor(named: "bottomBackgroundDay")
+    static let shadowBackgroundDay = UIColor(named: "shadowBackgroundDay")
+    
+    
+    //themes night
+    static let headerGobalNight = UIImage(named: "headers-global-night")
+    static let headerEventNight = UIImage(named: "headers-profile-night")
+    static let backgroundLoginNight = UIImage(named: "wallpeper")
+    static let backgroundNight = UIColor(named: "backgroundDay")
+    static let tintColorNight = UIColor(named: "tintColorDay")
+    static let topBackgroundNight = UIColor(named: "topBackgroundDay")
+    static let bottomBackgroundNight = UIColor(named: "bottomBackgroundDay")
+    static let shadowBackgroundNight = UIColor(named: "shadowBackgroundDay")
+    
+}
